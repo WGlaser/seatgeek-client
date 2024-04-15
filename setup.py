ed4d713a77d71b81fa155a6f9cb5495555d4b436
Wade Glaser <wade@innovateedunyc.org>
2024-04-15 15:54:32 -0400

    add setup.py for uploading to artifact registry

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("requirements.txt") as install_requires_file:
+    install_requires = install_requires_file.read().strip().split("\n")
+
+with open("requirements-dev.txt") as dev_requires_file:
+    dev_requires = dev_requires_file.read().strip().split("\n")
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="seatgeek-client",
+    description="Python client for SeatGeek",
+    author="Wade Glaser",
+    author_email="Wade.Glaser@Gmail.com",
+    url="https://github.com/WGlaser/seatgeek-client",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.10",
+    install_requires=install_requires,
+    packages=["seatgeek_client"],
+    version="0.0.1",
+)
