d6cac76e15858dc25ae825a953d34ce8f12adbdf
Wade Glaser <wade@innovateedunyc.org>
2024-04-22 16:37:26 -0400

    try versioneer again

--- seatgeek_client/__init__.py
+++ seatgeek_client/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
-from .seatgeek import SeatGeek
+
 
 from . import _version
 
-__version__ = _version.get_versions(verbose=True)["version"]
+__version__ = _version.get_versions()["version"]
